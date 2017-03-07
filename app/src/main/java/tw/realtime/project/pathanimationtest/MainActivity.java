194ce9f44bb3c93e8630b3a43485f91f8a08cac9
vexonelite <elite@realtime.tw>
2017-03-07 17:25:05 +0800

    remove ui and test code

--- app/src/main/java/tw/realtime/project/pathanimationtest/MainActivity.java
+++ app/src/main/java/tw/realtime/project/pathanimationtest/MainActivity.java
@@ -3,15 +3,12 @@ package tw.realtime.project.pathanimationtest;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
-    private DrawPathView mDrawPathView;
 
     private static String getLogTag () {
         return MainActivity.class.getSimpleName();
@@ -21,48 +18,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mDrawPathView = (DrawPathView) findViewById(R.id.drawPath);
-
-        View view = findViewById(R.id.drawLine);
-        if (null != view) {
-            view.setOnClickListener(new MyViewOnClickListener(DrawPathView.DrawCase.LINE));
-        }
-
-        view = findViewById(R.id.drawCicle);
-        if (null != view) {
-            view.setOnClickListener(new MyViewOnClickListener(DrawPathView.DrawCase.CUSTOM));
-        }
-
-        view = findViewById(R.id.drawAddArc);
-        if (null != view) {
-            view.setOnClickListener(new MyViewOnClickListener(DrawPathView.DrawCase.ADD_ARC));
-        }
-
-        view = findViewById(R.id.drawArcTo);
-        if (null != view) {
-            view.setOnClickListener(new MyViewOnClickListener(DrawPathView.DrawCase.ARC_TO));
-        }
-
-        view = findViewById(R.id.drawAddCircle);
-        if (null != view) {
-            view.setOnClickListener(new MyViewOnClickListener(DrawPathView.DrawCase.ADD_CIRCLE));
-        }
-
-        view = findViewById(R.id.drawAddPath);
-        if (null != view) {
-            view.setOnClickListener(new MyViewOnClickListener(DrawPathView.DrawCase.ADD_PATH));
-        }
-
-        view = findViewById(R.id.drawQuadTo);
-        if (null != view) {
-            view.setOnClickListener(new MyViewOnClickListener(DrawPathView.DrawCase.QUAD_TO));
-        }
-
-        view = findViewById(R.id.drawCublicTo);
-        if (null != view) {
-            view.setOnClickListener(new MyViewOnClickListener(DrawPathView.DrawCase.CIBLIC_TO));
-        }
 
         PathMotionMenuLayout pmmLayout = (PathMotionMenuLayout) findViewById(R.id.pathMotionMenu);
         if (null != pmmLayout) {
@@ -75,10 +30,12 @@ public class MainActivity extends AppCompatActivity {
             };
             pmmLayout.setParameters(
                     new PathMotionMenuLayout.Parameters()
+                            .setChildCount(6)
                             .setLayoutRadius(150)
                             .setDuration(300)
                             .setInterpolator(new AccelerateDecelerateInterpolator())
                             .setMainButtonResourceId(R.drawable.buddii_nav_ray_button)
+                            //.setOrbiterClickEffectFlag(false)
                             .setOrbiterResourceIdList(new ArrayList<>(Arrays.asList(resId)))
                             .setOrbiterClickListener(new MenuOrbiterClickCallback()));
         }
@@ -91,18 +48,5 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class MyViewOnClickListener implements View.OnClickListener {
-        private DrawPathView.DrawCase mDrawCase;
 
-        private MyViewOnClickListener (DrawPathView.DrawCase drawCase) {
-            mDrawCase = drawCase;
-        }
-
-        @Override
-        public void onClick(View view) {
-            view.setEnabled(false);
-            mDrawPathView.refresh(mDrawCase);
-            view.setEnabled(true);
-        }
-    }
 }
