b5904a7630ca2e0eed505dd90d3f322d54e6e318
vexonelite <elite@realtime.tw>
2017-04-12 17:31:19 +0800

    add interface OrbiterClickListener

---
+++ app/src/main/java/tw/realtime/project/pathanimationtest/OrbiterClickListener.java
@@ -0,0 +1,9 @@
+package tw.realtime.project.pathanimationtest;
+
+/**
+ * Created by vexonelite on 2017/4/12.
+ */
+
+public interface OrbiterClickListener {
+    void onOrbiterClicked (int position);
+}
