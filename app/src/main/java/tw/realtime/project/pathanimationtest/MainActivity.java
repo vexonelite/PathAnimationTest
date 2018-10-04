5ce7d6b0873cd6242d67db9a6349cc564230e916
vexonelite <elite@realtime.tw>
2018-10-04 17:51:49 +0800

    migrate to AndroidX

--- app/src/main/java/tw/realtime/project/pathanimationtest/MainActivity.java
+++ app/src/main/java/tw/realtime/project/pathanimationtest/MainActivity.java
@@ -1,7 +1,7 @@
 package tw.realtime.project.pathanimationtest;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 
 import java.util.ArrayList;
